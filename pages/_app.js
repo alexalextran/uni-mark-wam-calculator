dfb7379d8222ac39a38409a28bf7e9fd613c8a79
Alex Tran <alextran8385@gmail.com>
2022-12-01 13:11:14 +1100

    firebase auth functions

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.scss'
-
+import { AuthProvider } from "../Contexts/AuthContext";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <AuthProvider><Component {...pageProps} /></AuthProvider>
 }
 
 export default MyApp
