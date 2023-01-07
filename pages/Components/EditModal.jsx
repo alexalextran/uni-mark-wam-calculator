680edbe3c2aeed3fb7664ec9004871b53740da5d
Alex Tran <alextran8385@gmail.com>
2023-01-08 01:20:12 +1100

    modal now loads in with the selected subjects info as a default value to help users

--- pages/Components/EditModal.jsx
+++ pages/Components/EditModal.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import styles from "../../styles/EditModal.module.scss";
 
-const EditModal = () => {
+const EditModal = ({setedit, Name, credits}) => {
     return (
         <main className={styles.main}>
             <div className={styles.modal}>
             <div>
                 <form>
+
                  <span>
-                    <p>Name</p>
-                    <input></input>
+                    <h3>Name</h3>
+                    <input defaultValue={Name}></input>
                  </span>
+
+                 <span>
+                    <h3>Credits</h3>
+                    <input defaultValue={credits}></input>
+                 </span>
+
+               
                  
                  
                 </form>
+                <p>Note mark cannot be changed since it may conflict with the assignments if there are any</p>
             </div>
             <span>
-               <button>Cancel</button>
+               <button onClick={() => setedit(false)}>Cancel</button>
                <button>Submit</button> 
             </span>
             
